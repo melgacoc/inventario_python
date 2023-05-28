2f94b9a39e73f0e83f92de3b6d10918d450497b9
Cláudio <claudio19a@hotmail.com>
2023-05-28 16:56:41 -0300

    req 11

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,12 @@
+from inventory_report.inventory.inventory import Inventory
+import sys
+
+
 def main():
-    pass
+    try:
+        file_path, report_type = sys.argv[1:]
+        report = Inventory.import_data(file_path, report_type)
+        sys.stdout.write(report)
+
+    except (IndexError, KeyError, ValueError):
+        sys.stderr.write("Verifique os argumentos\n")
